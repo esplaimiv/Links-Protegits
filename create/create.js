fc228e835050695dd6dec4ee859cc7e311eb27e0
Esplai MIV Moviment Infantil de Vilafranca <esplaimiv@gmail.com>
2026-02-16 02:31:05 +0100

    Update

--- create/create.js
+++ create/create.js
@@ -1,25 +1,24 @@
 /**
- * Created by Jacob Strieb
- * May 2020
+ * Adaptació catalana – Esplai MIV
+ * Basat en el projecte original de Jacob Strieb (MIT License)
  */
 
 
-
 /*******************************************************************************
- * Helper Functions
+ * Funcions auxiliars
  ******************************************************************************/
 
-// Highlight the text in an input with a given id
+// Ressalta el text d’un input amb l’id indicat
 function highlight(id) {
   let output = document.querySelector("#" + id);
   output.focus();
-  output.select()
+  output.select();
   output.setSelectionRange(0, output.value.length + 1);
   return output;
 }
 
 
-// Validate all inputs, and display an error if necessary
+// Valida els inputs i mostra error si cal
 function validateInputs() {
   var inputs = document.querySelectorAll(".form .labeled-input input");
   for (let i = 0; i < inputs.length; i++) {
@@ -30,28 +29,26 @@ function validateInputs() {
     }
   }
 
-  // Extra check for older browsers for URL input. Not sure if necessary, since
-  // older browsers without built-in HTML5 validation may fail elsewhere.
   const url = document.querySelector("#url");
   let urlObj;
+
   try {
     urlObj = new URL(url.value);
   } catch {
     if (!("reportValidity" in url)) {
-      alert("URL invalid. Make sure to include 'http://' or 'https://' at the "
-          + "beginning.");
+      alert("L'URL no és vàlida. Assegureu-vos que comença amb 'http://' o 'https://'.");
     }
     return false;
   }
 
-  // Check for non-HTTP protocols; blocks them to prevent XSS attacks. Also
-  // allow magnet links for password-protected torrents.
   if (!(urlObj.protocol == "http:"
         || urlObj.protocol == "https:"
         || urlObj.protocol == "magnet:")) {
-    url.setCustomValidity("The link uses a non-hypertext protocol, which is "
-        + "not allowed. The URL begins with " + urlObj.protocol + " and may be "
-        + "malicious.");
+
+    url.setCustomValidity(
+      "L'enllaç utilitza un protocol no permès (" + urlObj.protocol +
+      "). Per motius de seguretat, només s'admeten enllaços http, https o magnet."
+    );
     url.reportValidity();
     return false;
   }
@@ -60,25 +57,23 @@ function validateInputs() {
 }
 
 
-// Perform encryption based on parameters, and return a base64-encoded JSON
-// object containing all of the relevant data for use in the URL fragment.
+// Genera el fragment xifrat
 async function generateFragment(url, passwd, hint, useRandomSalt, useRandomIv) {
   const api = apiVersions[LATEST_API_VERSION];
 
   const salt = useRandomSalt ? await api.randomSalt() : null;
   const iv = useRandomIv ? await api.randomIv() : null;
   const encrypted = await api.encrypt(url, passwd, salt, iv);
+
   const output = {
     v: LATEST_API_VERSION,
     e: b64.binaryToBase64(new Uint8Array(encrypted))
-  }
+  };
 
-  // Add the hint if there is one
   if (hint != "") {
     output["h"] = hint;
   }
 
-  // Add the salt and/or initialization vector if randomly generated
   if (useRandomSalt) {
     output["s"] = b64.binaryToBase64(salt);
   }
@@ -86,56 +81,58 @@ async function generateFragment(url, passwd, hint, useRandomSalt, useRandomIv) {
     output["i"] = b64.binaryToBase64(iv);
   }
 
-  // Return the base64-encoded output
   return b64.encode(JSON.stringify(output));
 }
 
 
 
 /*******************************************************************************
- * Main UI Functions
+ * Funcions principals de la interfície
  ******************************************************************************/
 
-// Activated when the "Encrypt" button is pressed
+// Quan es prem el botó "Encripta"
 async function onEncrypt() {
   if (!validateInputs()) {
     return;
   }
 
-  // Check that password is successfully confirmed
   const password = document.querySelector("#password").value;
-  const confirmPassword = document.querySelector("#confirm-password")
+  const confirmPassword = document.querySelector("#confirm-password");
   const confirmation = confirmPassword.value;
+
   if (password != confirmation) {
-    confirmPassword.setCustomValidity("Passwords do not match");
+    confirmPassword.setCustomValidity("Les contrasenyes no coincideixen.");
     confirmPassword.reportValidity();
     return;
   }
 
-  // Initialize values for encryption
   const url = document.querySelector("#url").value;
   const useRandomIv = document.querySelector("#iv").checked;
   const useRandomSalt = document.querySelector("#salt").checked;
+  const hint = document.querySelector("#hint").value;
 
-  const hint = document.querySelector("#hint").value
+  const encrypted = await generateFragment(
+    url,
+    password,
+    hint,
+    useRandomSalt,
+    useRandomIv
+  );
 
-  const encrypted = await generateFragment(url, password, hint, useRandomSalt,
-      useRandomIv);
-  const output = `https://jstrieb.github.io/link-lock/#${encrypted}`;
+  // Generació automàtica del domini actual
+  const baseUrl = window.location.origin + "/Links-Protegits/";
+  const output = `${baseUrl}#${encrypted}`;
 
   document.querySelector("#output").value = output;
   highlight("output");
 
-  // Adjust "Hidden Bookmark" link
-  document.querySelector("#bookmark").href = `https://jstrieb.github.io/link-lock/hidden/#${encrypted}`;
+  // Ajusta enllaç del marcador ocult
+  document.querySelector("#bookmark").href =
+    `${baseUrl}hidden/#${encrypted}`;
 
-  // Adjust "Open in New Tab" link
+  // Ajusta enllaç d’obertura directa
   document.querySelector("#open").href = output;
 
-  // Adjust "Get TinyURL" button
-  // document.querySelector("#tinyurl").value = output;
-
-  // Scroll to the bottom so the user sees where the bookmark was created
   window.scrollTo({
     top: document.body.scrollHeight,
     behavior: "smooth",
@@ -143,33 +140,32 @@ async function onEncrypt() {
 }
 
 
-// Activated when the "Copy" button is pressed
+// Quan es prem el botó "Copia"
 function onCopy(id) {
-  // Select and copy
   const output = highlight(id);
   document.execCommand("copy");
 
-  // Alert the user that the text was successfully copied
   const alertArea = document.querySelector(".alert");
-  alertArea.innerText = `Copied ${output.value.length} characters`;
+  alertArea.innerText = `S'han copiat ${output.value.length} caràcters.`;
   alertArea.style.opacity = "1";
-  setTimeout(() => { alertArea.style.opacity = 0; }, 3000);
 
-  // Deselect
+  setTimeout(() => {
+    alertArea.style.opacity = 0;
+  }, 3000);
+
   output.selectionEnd = output.selectionStart;
   output.blur();
 }
 
 
-// Activated when a user tries to disable randomization of the IV -- adds a
-// scary warning that will frighten off anyone with common sense, unless they
-// desperately need the URL to be a few characters shorter.
+// Avís si es desactiva la randomització de l'IV
 function onIvCheck(checkbox) {
   if (!checkbox.checked) {
-    checkbox.checked = !confirm("Please only disable initialization vector "
-        + "randomization if you know what you are doing. Disabling this is "
-        + "detrimental to the security of your encrypted link, and it only "
-        + "saves 20-25 characters in the URL length.\n\nPress \"Cancel\" unless "
-        + "you are very sure you know what you are doing.");
+    checkbox.checked = !confirm(
+      "Només desactiveu la generació aleatòria del vector d'inicialització " +
+      "si teniu coneixements tècnics suficients. Aquesta acció redueix la " +
+      "seguretat de l'enllaç protegit i només escurça lleugerament la seva longitud.\n\n" +
+      "Premeu «Cancel·la» si no n'esteu completament segurs."
+    );
   }
 }
